ad6267acb52dbccbafc15b2ac47c982425746439
Juan Escobar <jcarriola83@gmail.com>
2023-03-15 10:41:27 +0000

    Feature Spec Add to Cart

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,12 @@
+describe('Jungle app', () => {
+  beforeEach(() => {
+    
+    cy.visit('http://localhost:3000')
+  })
+  
+  it("add product to cart and cart increases by one", () => {
+  cy.get(".products article").first().contains('Add').click({force: true})
+  cy.get('.nav-item.end-0').should('have.text', '\n           \n          My Cart (1) \n        ')
+});
+
+})
